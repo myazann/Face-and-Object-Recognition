b6d1e19f9173314e3e7401f741303dfd632a752f
Mert Yazan <36661980+myazann@users.noreply.github.com>
2021-03-15 19:59:48 +0300

    Update Face-Recognition.py

--- Face-Recognition.py
+++ Face-Recognition.py
@@ -10,10 +10,7 @@ from facenet_pytorch import MTCNN, InceptionResnetV1
 import torch
 from torch.utils.data import DataLoader
 from torchvision import datasets
-import numpy as np
-import pandas as pd
 import cv2
-import csv
 
 workers = 0 if os.name == 'nt' else 4
 
